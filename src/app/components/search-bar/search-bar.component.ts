a4c601badb2536192be54333b3861acd78d2c951
dgkhoa89 <dgkhoa89@gmail.com>
2022-03-25 21:46:11 +0700

    update css and add submit function for form in search bar

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Form } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
@@ -11,5 +12,7 @@ export class SearchBarComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  onSubmit(form: Form){
 
+  }
 }
